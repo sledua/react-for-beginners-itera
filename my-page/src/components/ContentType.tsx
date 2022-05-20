6698fc02a3b3b00a5dec2bd347b4e8aa96e2ff2f
Artem <art.sled.ua@gmail.com>
2022-05-20 23:39:41 +0300

    fiet
    front path for links row? and fix fa lib

--- my-page/src/components/ContentType.tsx
+++ my-page/src/components/ContentType.tsx
@@ -1,20 +1,36 @@
 import React, { FC } from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faLinkedin, faTelegramPlane, faGithub } from "@fortawesome/free-brands-svg-icons"
 
-const ContentType: FC = (props) => {
+interface ContentTypeProps {}
+const ContentType: FC<ContentTypeProps> = (props) => {
+	console.log(faTelegramPlane)
 	return (
 		<div className="card-page">
 			<section className="padding-6">
 				<div className="container content">
 					<div className="row grid">
 						<div className="thumb">
-							<img src={require("../assets/img/autor-thumb.png")} alt="autor-thumb" className="avatar"/>
+							<img src={require("../assets/img/autor-thumb.png")} alt="autor-thumb" className="avatar" />
 						</div>
 						<div className="information">
 							<h3>Artem</h3>
 							<div className="links">
-								<a href="/">Links</a>
+								<a href="/" target="_blank" rel="noopener noreferrer">
+									<FontAwesomeIcon icon={faTelegramPlane} />
+								</a>
+								<a href="/" target="_blank" rel="noopener noreferrer">
+									<FontAwesomeIcon icon={faLinkedin} />
+								</a>
+								<a href="/" target="_blank" rel="noopener noreferrer">
+								<FontAwesomeIcon icon={faGithub} />
+								</a>
 							</div>
-							<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta placeat laudantium neque eveniet tempora vel consectetur, asperiores fuga, quibusdam expedita, voluptatibus explicabo esse minima! Officia nulla quod modi quis consequatur?</p>
+							<p>
+								Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta placeat laudantium neque eveniet
+								tempora vel consectetur, asperiores fuga, quibusdam expedita, voluptatibus explicabo esse minima!
+								Officia nulla quod modi quis consequatur?
+							</p>
 						</div>
 					</div>
 				</div>
