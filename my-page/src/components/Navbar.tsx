0de42327be160073eb353fc1647b1955ecac6d64
Artem <art.sled.ua@gmail.com>
2022-05-21 23:59:38 +0300

    fix
    fix mobile sciling

--- my-page/src/components/Navbar.tsx
+++ my-page/src/components/Navbar.tsx
@@ -31,19 +31,19 @@ const Navbar: FC<NavigationProps> = (props) => {
 				<div className="container flex">
 					<Link to={props.navigation[4].path}>{props.logo}</Link>
 					<button className="menu-toggler" aria-expanded="false"></button>
-					<div>
+					
 						<ul className="navbar-list flex">{renderNavigation()}</ul>
-					</div>
+					
 				</div>
 			</nav>
-			<header>
-				<div className="header">
+			<header className="header">
+				
 					<span></span>
 					<div className="container">
 						<h1>{props.logo}</h1>
 						<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum, officiis?</p>
 					</div>
-				</div>
+				
 			</header>
 		</>
 	)
