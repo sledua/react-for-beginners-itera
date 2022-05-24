1816f8cba5f5f101f78609ca8948b8282449cf87
Artem <art.sled.ua@gmail.com>
2022-05-25 01:42:28 +0300

    fix
    time to coffe

--- my-page/src/components/Header.tsx
+++ my-page/src/components/Header.tsx
@@ -1,37 +1,36 @@
-import { FC } from "react"
+import { FC, useContext } from "react"
 import { HeaderProps } from "../types/types"
 import styled from "styled-components"
 import home from "../assets/img/header-bg.jpg"
 import task from "../assets/img/header-task-bg.jpg"
 import blog from "../assets/img/header-blog-bg.jpg"
 import aboutMe from "../assets/img/header-about-bg.jpg"
-
+const Headers = styled.header`
+	padding: 0;
+	position: relative;
+	overflow: hidden;
+	display: flex;
+	align-items: center;
+	background-size: cover;
+	background-position: 50%;
+	min-height: 70vh;
+	background-image: ${() => {
+		switch (props.page) {
+			case "home":
+				return `url(${home})`
+			case "autor":
+				return`url(${aboutMe})`
+			case "task":
+				return `url(${task})`
+			case "blog":
+				return `url(${blog})`
+			default:
+				return `url(${home})`
+		}
+	}}
+`
 const Header: FC<HeaderProps> = (props) => {
-	const Headers = styled.header`
-		padding: 0;
-		position: relative;
-		overflow: hidden;
-		display: flex;
-		align-items: center;
-		background-size: cover;
-		background-position: 50%;
-		min-height: 70vh;
-		background-image: ${() => {
-			switch (props.page) {
-				case "home":
-					return `url(${home})`
-				case "autor":
-					return`url(${aboutMe})`
-				case "task":
-					return `url(${task})`
-				case "blog":
-					return `url(${blog})`
-				default:
-					return `url(${home})`
-			}
-		}}
-	`
-	console.log(props.page)
+	const {styleForheader} = useContext{Glo} 
 	return (
 		<Headers className="header">
 			<span></span>
